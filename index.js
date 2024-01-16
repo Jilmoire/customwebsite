b4fb7350081e737a2b69152d713ef00fac65e2e8
Jilmoire <guilmarlopez2007@gmail.com>
2024-01-16 13:31:23 +0800

    push

---
+++ index.js
@@ -0,0 +1,18 @@
+function changetext()
+{
+    document.getElementById("desc").innerHTML="General mathematics is an introductory, basic, exploratory course in which the simple and significant principles of arithmetic, algebra, intuitive geometry, statistics, and numerical trigonometry are taught so as to emphasize their natural and numerous interrelations."
+}
+function changetext2()
+{
+    document.getElementById("desc").innerHTML="In mathematics education, precalculus is a course, or a set of courses, that includes algebra and trigonometry at a level which is designed to prepare students for the study of calculus, thus the name precalculus."
+}
+
+function changecontent()
+{
+    document.getElementById("learnmat").innerHTML="<div class=display;><div class=pcal;>2nd Quarter Pre-Calculus:</div><br><div class=precalsel><a href=2angleId.html><p>Module 1: Double Angle Identities</p></a><p>Module 1.2: Half Angle Identities</p></div>"
+}
+
+function changecontent2()
+{
+    document.getElementById("learnmat").innerHTML="<div class=display; id=learnmat>2nd Quarter General Mathematics:</div><br><div class=genmathsel>Module 1: One-to-one Function<p><a href=InvFunc.html>Module 1.2: Inverse Functions</p></a></div>"
+}
